efec7e9fc5681b0eaea2a772a50b95efb7f6dd7e
zainab66 <zainab66r@gmail.com>
2023-05-18 11:38:27 -0400

    create components folder, create component subfolders with jsx and css files. Export all from index.js and import on one line to app.js

---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './brand.css';
+
+const Brand = () => {
+  return <div></div>;
+};
+
+export default Brand;
---
+++ src/components/cta/CTA.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './cta.css';
+
+const CTA = () => {
+  return <div></div>;
+};
+
+export default CTA;
