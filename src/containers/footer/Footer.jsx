37129516f61fa7a37f94b2de1aa526f7b0a8f9dd
zainab66 <zainab66r@gmail.com>
2023-05-18 22:20:36 -0400

    add all content to footer and begin CSS styling

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -16,7 +16,30 @@ const Footer = () => {
       <div className="gpt3__footer-links">
         <div className="gpt3__footer-links_logo">
           <img src={gpt3Logo} alt="logo" />
+          <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
         </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Get in Touch</h4>
+          <p>Crechterwoord K12 182 DK Alknjkcb</p>
+          <p>085-132567</p>
+          <p>info@payme.net</p>
+        </div>
+      </div>
+      <div className="gpt3__footer-copyright">
+        <p>© 2021 GPT-3. All rights reserved.</p>
       </div>
     </div>
   );
