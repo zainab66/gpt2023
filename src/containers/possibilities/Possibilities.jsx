cdc7330b9a4a94a2fde661e52fead00903eeb339
zainab66 <zainab66r@gmail.com>
2023-05-18 21:53:13 -0400

    add content to possibilities component

--- src/containers/possibilities/Possibilities.jsx
+++ src/containers/possibilities/Possibilities.jsx
@@ -8,6 +8,19 @@ const Possibilities = () => {
       <div className="gpt3__possibility-image">
         <img src={possibilityImage} alt="possibility" />
       </div>
+      <div className="gpt3__possibility-content">
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className="graident-text">
+          The possibilites are beyond our imagination
+        </h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <h4>Request Early Access to Get Started</h4>
+      </div>
     </div>
   );
 };
