02c4cbf40632f7a153d7da7a148d53b26891bac6
zainab66 <zainab66r@gmail.com>
2023-05-18 22:05:13 -0400

    add blog export file inside imports for oneline use

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import './article.css';
 
-const Article = () => {
-  return <div></div>;
+const Article = ({ imgUrl }) => {
+  return (
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blong-container_article-image">
+        <img src={imgUrl} alt="blog image" />
+      </div>
+    </div>
+  );
 };
 
 export default Article;
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
 import './blog.css';
+import { blog01, blog02, blog03, blog04, blog05 } from './imports';
+import { Article } from '../../components';
 
 const Blog = () => {
-  return <div></div>;
+  return (
+    <div className="gpt3__blog section__padding" id="blog">
+      <div className="gpt3__blog-heading">
+        <h1 className="gradient__text">
+          A lot is happening. We are blogging about it.
+        </h1>
+      </div>
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-container_groupA">
+          <Article />
+        </div>
+        <div className="gpt3__blog-container_groupB">
+          <Article />
+          <Article />
+          <Article />
+          <Article />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
