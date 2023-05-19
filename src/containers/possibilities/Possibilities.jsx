12e981974b660bcddbc22d2d3bff114db08dcbe7
zainab66 <zainab66r@gmail.com>
2023-05-18 21:52:11 -0400

    import image from asset and add code to render it

--- src/containers/possibilities/Possibilities.jsx
+++ src/containers/possibilities/Possibilities.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import './possibilities.css';
+import possibilityImage from '../../assets/possibility.png';
 
 const Possibilities = () => {
-  return <div></div>;
+  return (
+    <div className="gpt3__possibility section__padding" id="possibility">
+      <div className="gpt3__possibility-image">
+        <img src={possibilityImage} alt="possibility" />
+      </div>
+    </div>
+  );
 };
 
 export default Possibilities;
