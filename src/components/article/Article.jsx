13b8e7c20a16ed1b2ae184e7c3454df743929318
zainab66 <zainab66r@gmail.com>
2023-05-18 22:11:23 -0400

    add date and title props to Article and pass into new div under p and h3 tag

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './article.css';
 
-const Article = ({ imgUrl }) => {
+const Article = ({ imgUrl, date, title }) => {
   return (
     <div className="gpt3__blog-container_article">
       <div className="gpt3__blong-container_article-image">
-        <img src={imgUrl} alt="blog image" />
+        <img src={imgUrl} alt="blog" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
       </div>
     </div>
   );
